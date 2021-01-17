46491e9283346c2c874bda0e802273c0cdc8db80
w99910 <65523475+w99910@users.noreply.github.com>
2021-01-17 23:58:41 +0630

    Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
-        <script src="{{asset('js/app.js')}}" defer></script>
+        <title>DashBoard</title>
+        <link rel="stylesheet" href="{{secure_asset('css/app.css')}}"/>
+        <script src="{{secure_asset('js/app.js')}}" defer></script>
     </head>
     <body class="antialiased h-screen w-screen bg-gray-300">
     <div id="app" class="w-full h-full">
