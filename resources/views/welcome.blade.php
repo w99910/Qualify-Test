14cd1fee5216d35971483e1ccfcf58e53816f447
w99910 <65523475+w99910@users.noreply.github.com>
2021-01-26 18:53:45 +0630

    Debug-2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>DashBoard</title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
-        <script src="{{asset('js/app.js')}}" defer></script>
+        <link rel="stylesheet" href="{{secure_asset('css/app.css')}}"/>
+        <script src="{{secure_asset('js/app.js')}}" defer></script>
     </head>
     <body class="font-poppins antialiased min-h-screen sm:h-screen sm:w-screen overflow-auto sm:overflow-hidden m-0 p-0">
     <div id="app" class="w-full h-full">
